21a28f97a72f768bcf0a9bf1ea2a5944adbab8b8
makkawy16 <yousef.elmakkawy@gmail.com>
2025-05-26 12:00:44 +0300

    Add Model, DbContext and connection string on Sql Server

---
+++ CQRS/Data/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions):base(dbContextOptions) { }
+    }
+}
